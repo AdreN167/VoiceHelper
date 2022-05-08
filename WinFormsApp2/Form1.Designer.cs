7b680d8f08486b0d89ba84bacc93796012f7a914
b1skeetz <damirk120404@gmail.com>
2022-05-08 17:33:22 +0600

    dataGridView

--- WinFormsApp2/Form1.Designer.cs
+++ WinFormsApp2/Form1.Designer.cs
@@ -28,19 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView_DataBase = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_DataBase)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView_DataBase
+            // 
+            this.dataGridView_DataBase.AllowUserToAddRows = false;
+            this.dataGridView_DataBase.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_DataBase.Location = new System.Drawing.Point(380, 29);
+            this.dataGridView_DataBase.Name = "dataGridView_DataBase";
+            this.dataGridView_DataBase.RowTemplate.Height = 25;
+            this.dataGridView_DataBase.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView_DataBase.Size = new System.Drawing.Size(375, 311);
+            this.dataGridView_DataBase.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView_DataBase);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_DataBase)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView_DataBase;
     }
 }
