105621b17167fc1cdd38b7ed05e1565e2840704d
b1skeetz <damirk120404@gmail.com>
2022-05-08 17:57:36 +0600

    testing

--- WinFormsApp2/Form1.Designer.cs
+++ WinFormsApp2/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.dataGridView_DataBase = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_DataBase)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,11 +44,22 @@
             this.dataGridView_DataBase.Size = new System.Drawing.Size(375, 311);
             this.dataGridView_DataBase.TabIndex = 0;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(211, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView_DataBase);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -60,5 +72,6 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView_DataBase;
+        private System.Windows.Forms.Button button1;
     }
 }
