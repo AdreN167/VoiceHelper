f648b5534f425dd5145270058b1ee68d354b7fb6
b1skeetz <damirk120404@gmail.com>
2022-05-11 23:10:11 +0600

    voice error

--- WinFormsApp2/Form1.Designer.cs
+++ WinFormsApp2/Form1.Designer.cs
@@ -28,84 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView_DataBase = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_DataBase)).BeginInit();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.базаДанныхToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.настройкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dataGridView_DataBase
+            // menuStrip1
             // 
-            this.dataGridView_DataBase.AllowUserToAddRows = false;
-            this.dataGridView_DataBase.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView_DataBase.Location = new System.Drawing.Point(380, 29);
-            this.dataGridView_DataBase.Name = "dataGridView_DataBase";
-            this.dataGridView_DataBase.RowTemplate.Height = 25;
-            this.dataGridView_DataBase.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView_DataBase.Size = new System.Drawing.Size(375, 311);
-            this.dataGridView_DataBase.TabIndex = 0;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.базаДанныхToolStripMenuItem,
+            this.настройкиToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
             // 
-            // button1
+            // файлToolStripMenuItem
             // 
-            this.button1.Location = new System.Drawing.Point(211, 200);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
             // 
-            // panel1
+            // базаДанныхToolStripMenuItem
             // 
-            this.panel1.Location = new System.Drawing.Point(100, 57);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
-            this.panel1.TabIndex = 2;
+            this.базаДанныхToolStripMenuItem.Name = "базаДанныхToolStripMenuItem";
+            this.базаДанныхToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
+            this.базаДанныхToolStripMenuItem.Text = "База Данных";
             // 
-            // button2
+            // настройкиToolStripMenuItem
             // 
-            this.button2.Location = new System.Drawing.Point(85, 270);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(176, 269);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.настройкиToolStripMenuItem.Name = "настройкиToolStripMenuItem";
+            this.настройкиToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.настройкиToolStripMenuItem.Text = "Настройки";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView_DataBase);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_DataBase)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView_DataBase;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem базаДанныхToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem настройкиToolStripMenuItem;
     }
 }
--- WinFormsApp2/Form1.cs
+++ WinFormsApp2/Form1.cs
@@ -7,24 +7,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech.Recognition;
+
 
 namespace WinFormsApp2
 {
     public partial class Form1 : Form
     {
+        SpeechRecognitionEngine speechEngine;
         public Form1()
         {
             InitializeComponent();
+
+            System.Globalization.CultureInfo culture = new System.Globalization.CultureInfo("ru-ru"); /* Язык распознования */
+            speechEngine = new SpeechRecognitionEngine(culture);
+            speechEngine.LoadGrammar(new DictationGrammar()); // Dictation mode.
+            speechEngine.SetInputToDefaultAudioDevice();
+
+            speechEngine.SpeechRecognized += SpeechEngine_SpeechRecognized; /* Событие завершения распознования текста */
+
+            Choices choices = new Choices(); /* Выбор распознования (комманды) */
+            choices.Add(new string[] { "Открой", "файл", "test" });
+
+            GrammarBuilder grammarBuilder = new GrammarBuilder();
+            grammarBuilder.Append(choices);
+            Grammar grammar = new Grammar(grammarBuilder);
+            speechEngine.LoadGrammar(grammar);
+
+            speechEngine.SpeechRecognized += (sender, args) =>
+            {
+                // Ваш обработчик.
+                MessageBox.Show(string.Join(", ", args.Result.Words));
+            };
         }
 
+
+        private void SpeechEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            MessageBox.Show(e.Result.Text);
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
             
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            dataGridView_DataBase.Visible = false;
         }
     }
 }
